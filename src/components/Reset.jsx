b235c5b429ce1dc26a05a1693242c3aa0ccb1807
russellduckworth <russ.duckworth@gmail.com>
2020-07-14 21:07:17 -0700

    Adds dramatic intro
    Planet and stars fade in. UI flys up from below. Cooler looking clouds. Reset button upgrade.

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
 import store from '../LocalStore'
+import util from '../AppUtil'
 import app from '../app'
 
 export default class Reset extends Component
 {
 	click() {
 		store.deleteEverything();
-		app.reloadApp();
+		util.$('reset-button').innerText = "So long and thanks for all the fish"
+		util.wait(1200)
+		.then( () => {
+			app.reloadApp();
+		})
 	}
 
 	render() {
 		return (
 			<div >
 				<div style={{height: '444px'}}></div>
-				<button className='button'
+				<button
+					id='reset-button'
+					className='button'
 					onClick={(e) => this.click(e)}
 				>Reset Planet</button>
 				<div style={{height: '222px'}}></div>
